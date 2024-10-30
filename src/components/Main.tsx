5d5f848ef52d846293dac70cf6c5747b4c91aae1
sirdest <akinro.destined@gmail.com>
2024-10-31 00:36:20 +0100

    updates

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 
 const Main = () => {
   const [items, setItems] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
+  const [errorLoading, setErrorLoading] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const itemsPerPage = 10;
+  const totalPages = Math.ceil(items.length / itemsPerPage);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -15,6 +18,7 @@ const Main = () => {
         setItems(coinlore.data);
       } catch (error) {
         console.error("Error fetching items:", error);
+        setErrorLoading(true);
       } finally {
         setIsLoading(false);
       }
@@ -26,37 +30,42 @@ const Main = () => {
   const indexOfFirstItem = currentPage * itemsPerPage;
   const indexOfLastItem = indexOfFirstItem + itemsPerPage;
   const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
-  console.log("currentItems", currentItems);
 
-  //   const handleNext = () => {
-  //     if ((currentPage + 1) * itemsPerPage < items.length) {
-  //       setCurrentPage(currentPage + 1);
-  //     }
-  //   };
+  const handleNext = () => {
+    if ((currentPage + 1) * itemsPerPage < items.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
 
-  //   const handlePrevious = () => {
-  //     if (currentPage > 0) {
-  //       setCurrentPage(currentPage - 1);
-  //     }
-  //   };
+  const handlePrevious = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   return (
-    <div className="bg-green-500 w-full h-screen flex justify-center p-12">
-      <div className="bg-white w-[500px] h-[500px] border border-gray-300 flex">
-        {isLoading ? (
-          <p className="text-center m-auto">Loading...</p>
+    <div className="w-full h-screen flex justify-center p-0 md:p-12">
+      <div className="bg-white w-full md:w-[650px] h-fit min-h-[400px] border border-gray-300 flex">
+        {errorLoading ? (
+          <p className="text-center m-auto text-[13px]">
+            Loading failed! Check Iternet connection and try again.
+          </p>
+        ) : isLoading ? (
+          <p className="text-center m-auto text-[13px]">
+            Loading... Please Wait!
+          </p>
         ) : (
-          <div>
+          <div className="w-full h-full flex flex-col">
             <table className="h-full w-full">
               <thead className="bg-white w-full border-b">
                 <tr className="flex w-full justify-between px-4 py-3 text-left text-gray-700 font-semibold">
-                  <th className="w-1/4">Coin</th>
-                  <th className="w-1/4">Code</th>
-                  <th className="w-1/4">Price</th>
-                  <th className="w-1/4">Total Supply</th>
+                  <th className="w-1/4">💰Coin</th>
+                  <th className="w-1/4">📄Code</th>
+                  <th className="w-1/4">🤑Price</th>
+                  <th className="w-1/4">📉Total Supply</th>
                 </tr>
               </thead>
-              <tbody className="h-full w-full">
+              <tbody className="h-full w-full text-[13px]">
                 {currentItems.map(
                   ({ id, name, symbol, price_usd, tsupply }) => {
                     return (
@@ -74,7 +83,28 @@ const Main = () => {
                 )}
               </tbody>
             </table>
-            <div className="flex justify-between "></div>
+            <div className="w-full flex justify-between p-2">
+              <div className="flex-1">
+                {currentPage >= 1 && (
+                  <button
+                    onClick={handlePrevious}
+                    className="border-none outline-none flex gap-2 items-center"
+                  >
+                    <AiOutlineArrowLeft className="text-[12px]" /> Prev
+                  </button>
+                )}
+              </div>
+              <div className="flex-1 flex justify-end">
+                {currentPage < totalPages - 1 && (
+                  <button
+                    onClick={handleNext}
+                    className="border-none outline-none flex gap-2 items-center"
+                  >
+                    Next <AiOutlineArrowRight className="text-[12px]" />
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
         )}
       </div>
